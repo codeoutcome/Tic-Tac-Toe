efeb385e7d0a130eb54b455dffadac0bf3e3a39b
Sam Stimpson <hello+code@samstimpson.com>
2024-03-13 17:07:42 +0000

    We are getting somewhere baby

--- src/App.js
+++ src/App.js
@@ -10,12 +10,25 @@ function Square({value, onSquareClick}) {
  * @returns {JSX.Element} The rendered board component.
  */
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  console.log(squares);
+  
   function handleClick(i) {
+    // console.log(squares[i])
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
