8b058b61b8d26808ef231ef69525c21f86da82b8
Sam Stimpson <hello+code@samstimpson.com>
2024-03-13 01:25:10 +0000

    Making an interactive component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 
+function Square() {
+  const [value, setValue] = useState(null);
 
-function Square({value}) {
+  function handleClick() {
+    setValue('X');
+  }
   return (
-    <button className="square">{value}</button>
+    <button onClick={handleClick} className="square">{value}</button>
   );
 }
 
@@ -15,19 +20,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
