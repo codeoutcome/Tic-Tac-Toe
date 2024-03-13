0460f70366bc11e44e2797145972f8f4182e4a92
Sam Stimpson <hello+code@samstimpson.com>
2024-03-13 01:17:41 +0000

    abstracted sqaure from the board passed prop value through

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+
+
+function Square({value}) {
+  return (
+    <button className="square">{value}</button>
+  );
+}
+
 /**
  * Renders a board with 9 buttons arranged in a 3x3 grid.
  * Each button represents a square on the board.
  * @returns {JSX.Element} The rendered board component.
  */
-export default function Boardgit () {
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
     </>
   );
